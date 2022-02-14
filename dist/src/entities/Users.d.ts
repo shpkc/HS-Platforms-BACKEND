e56212dbb231cb81b49ca8611bab0a54e27042bd
Park Si Hyung <shpkc@naver.com>
2022-02-14 18:03:16 +0900

    chore : delete build

--- dist/src/entities/Users.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Users {
-    id: number;
-    email: string;
-    nickname: string;
-    password: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deletedAt: Date | null;
-}
